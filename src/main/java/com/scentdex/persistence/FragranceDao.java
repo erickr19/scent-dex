f37a608ce426ae0f351b7762888d77f5fc03a6b6
Erick <ereyes3@madisoncollege.edu>
2023-02-14 11:35:45 -0600

    Relocating persistence package from outside com.scentdex to inside.

--- src/main/java/com/scentdex/persistence/FragranceDao.java
+++ src/main/java/com/scentdex/persistence/FragranceDao.java
@@ -1,17 +1,8 @@
-package persistence;
+package com.scentdex.persistence;
 
-import com.scentdex.entity.Fragrance;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.List;
 
 /**
  * Fragrance Data Access Object
--- src/main/java/com/scentdex/persistence/SessionFactoryProvider.java
+++ src/main/java/com/scentdex/persistence/SessionFactoryProvider.java
@@ -1,4 +1,4 @@
-package persistence;
+package com.scentdex.persistence;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
